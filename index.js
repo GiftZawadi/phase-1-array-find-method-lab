64e3da1d7e286bffc977384c0ebbb409fa976d7b
Your Full Name <gzomandi07@gmail.com>
2024-03-27 08:41:31 +0300

    compleete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function superbowlWin(superbowls) {
+    const winningYearObj = superbowls.find(superbowl => superbowl.result === "W");
+    return winningYearObj ? winningYearObj.year : undefined;
+}
